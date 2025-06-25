ad1c3a2c2529100884f69eca97ed1d6bff135622
Alfonso <alfomosque22@gmail.com>
2025-06-25 02:55:17 -0500

    ok

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 export default function CustomHead() {
   return (
     <Head>
+      <title>Servicios IT | Alfonso Mosquera</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
